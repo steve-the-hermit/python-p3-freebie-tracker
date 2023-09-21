9167ec1d6df94752f8ef9dfbf99762bae5963c9f
steve-the-hermit <stevemacharia309@gmail.com>
2023-09-22 01:09:20 +0300

    done

--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,58 @@
-#!/usr/bin/env python3
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
-# Script goes here!
+Base = declarative_base()
+
+class Freebie(Base):
+    __tablename__ = 'freebies'
+
+    id = Column(Integer, primary_key=True)
+    item_name = Column(String)
+    value = Column(Integer)
+    dev_id = Column(Integer, ForeignKey('devs.id'))
+    company_id = Column(Integer, ForeignKey('companies.id'))
+
+    # Define relationships
+    dev = relationship('Dev', back_populates='freebies')
+    company = relationship('Company', back_populates='freebies')
+
+    def print_details(self):
+        return f"{self.dev.name} owns a {self.item_name} from {self.company.name}"
+
+class Company(Base):
+    __tablename__ = 'companies'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    founding_year = Column(Integer)
+
+    # Define relationships
+    freebies = relationship('Freebie', back_populates='company')
+    devs = relationship('Dev', secondary='freebies', back_populates='companies')
+
+    @classmethod
+    def oldest_company(cls):
+        return cls.query.order_by(cls.founding_year).first()
+
+    def give_freebie(self, dev, item_name, value):
+        freebie = Freebie(dev=dev, company=self, item_name=item_name, value=value)
+        dev.freebies.append(freebie)
+        self.freebies.append(freebie)
+
+class Dev(Base):
+    __tablename__ = 'devs'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    # Define relationships
+    freebies = relationship('Freebie', back_populates='dev')
+    companies = relationship('Company', secondary='freebies', back_populates='devs')
+
+    def received_one(self, item_name):
+        return any(freebie.item_name == item_name for freebie in self.freebies)
+
+    def give_away(self, other_dev, freebie):
+        if freebie in self.freebies:
+            freebie.dev = other_dev
